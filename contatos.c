bb673e92df3098acb5fae2d13a5564adabcb9950
Linejesus <161092862+Linejesus@users.noreply.github.com>
2024-04-22 23:25:05 +0000

    Função de adicionar contatos

---
+++ contatos.c
@@ -0,0 +1,34 @@
+#include <stdio.h> // Importa a biblioteca stdio.h para que possamos utilizar as funções de entrada e saída padrão de dados
+#include <string.h> // Importa a biblioteca string.h para que possamos utilizar as funções de manipulação de strings
+#include "contatos.h" // Importa o arquivo tarefas.h para que possamos utilizar as funções declaradas nele
+
+ERRO adicionar(Agenda contatos[], int *pos){ // Função de adicionar contato, recebe um array de contatos e um ponteiro para a posição atual dos contatos
+  if(*pos >= LIMITE_AGENDA) // Verificando erro de limite de contatos atingido
+    return MAX_CONTATOS; // Retornando código de ultrapassou do limite de contatos
+
+  clearBuffer(); // Chamando a função clearBuffer para limpar o buffer do teclado
+  printf("\nDigite o nome do contato: "); // Pedindo para o usuário nos informar o nome do contato
+  fgets(contatos[*pos].nome, 20, stdin); // Lendo o nome digitado pelo usuário
+  clearBuffer(); // Chamando a função clearBuffer para limpar o buffer do teclado
+
+  printf("Digite o sobrenome do contato: "); // Pedindo para o usuário nos informar o sobrenome do contato
+  fgets(contatos[*pos].sobrenome, 100, stdin); // Lendo o sobrenome digitado pelo usuário
+  clearBuffer(); // Chamando a função clearBuffer para limpar o buffer do teclado
+
+  printf("Digite o email do contato: "); // Pedindo para o usuário nos informar o email do contato
+  fgets(contatos[*pos].email, 100, stdin); // Lendo o email digitado pelo usuário
+  clearBuffer(); // Chamando a função clearBuffer para limpar o buffer do teclado
+
+  printf("Digite o telefone do contato: "); // Pedindo para o usuário nos informar o telefone do contato
+  scanf("%ld", &contatos[*pos].telefone); // Lendo o telefone digitado pelo usuário
+  clearBuffer(); // Chamando a função clearBuffer para limpar o buffer do teclado
+
+  *pos = *pos + 1; // Incrementando a posição para o próximo contato
+
+  return OK; // Retornando código de sucesso na execução
+} // Fechando função de adicionar contato
+
+void clearBuffer(){ // Função de Limpar Buffer, para evitar o erro gerado ao tentar obter a entrada de informações do usuário
+    int c;  // Declarando variável do tipo inteiro para armazenar o caractere lido do buffer
+    while ((c = getchar()) != '\n' && c != EOF); // Loop para limpar o buffer do teclado, comparando o valor lido com o valor \n (New Line), e também comparando com o valor EOF (End Of File), para garantir que ele não continue tentando ler quando chegar ao final do arquivo
+} // Fechando função de limpar buffer
---
+++ contatos.h
@@ -0,0 +1,17 @@
+// Definindo uma "variável" global e constante para a quantidade máxima de contatos que podem ser armazenados na agenda
+#define LIMITE_AGENDA 255 
+
+typedef struct { // Definindo uma estrutura para armazenar as informações dentro de cada contato
+    char nome[20]; // Declarando variável do tipo string com 20 caracteres para armazenar o nome do contato
+    char sobrenome[100]; // Declarando variável do tipo string com 100 caracteres para armazenar o sobrenome do contato
+    char email[100]; // Declarando variável do tipo string com 100 caracteres para armazenar o email do contato
+    long int telefone; // Declarando variável do tipo inteiro longo para armazenar o telefone do contato
+} Agenda; // Definindo o nome da estrutura como Agenda
+
+typedef enum {OK, MAX_CONTATOS, SEM_CONTATOS, CONTATO_NAO_ENCONTRADO, ABRIR, FECHAR, ESCREVER, LER} ERRO;// Definindo uma enumeração de códigos de erro para ficar mais legível de identificar o que cada código significa
+
+typedef ERRO (*funcao)(Agenda[], int*); // Definindo um tipo de função que recebe um array de contatos e um ponteiro para a posição atual dos contatos, e retorna um código de erro
+
+ERRO adicionar(Agenda contatos[], int *pos); // Assinatura da função de adicionar contato
+
+void clearBuffer(); // Assinatura da função de limpar buffer
